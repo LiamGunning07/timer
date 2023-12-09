ccfd68630332d19a40d776832744fdf2aeb9ff46
LiamGunning07 <liamgunning07@gmail.com>
2023-12-08 17:00:49 -0700

    "Implement simple timer with command line argument support and edge case handling"

--- timer1.js
+++ timer1.js
@@ -1,18 +1,18 @@
 const args = process.argv.slice(2);
+const seconds = args.filter(arg => !isNaN(arg) && arg >= 0).sort(function(a, b) {
+  return a - b;
+});
+
 const timer = function(time) {
   for (let i = 0; i < time.length; i++) {
     setTimeout(() => {
       process.stdout.write('\x07');
     }, time[i] * 1000); // Multiply by 1000 to convert seconds to milliseconds
   }
+};
+
+if (seconds.length > 0) {
+  timer(seconds);
+} else {
+  console.log("No valid positive numbers provided. Ending program.");
 }
-const seconds = args.sort(function(a, b) {
-  return a - b;
-});
-
-timer(seconds);
-
-
-
-
-
